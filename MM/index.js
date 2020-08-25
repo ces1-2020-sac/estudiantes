f2288a60a2dd60d5d2af20e46392f7a599a05594
ManuMarti <manuela3103mch@gmail.com>
2020-08-25 13:56:37 -0500

    Cambios en mi carpeta de Manuela

--- MM/index.js
+++ MM/index.js
@@ -1,4 +1,4 @@
 /**
  * 1. git . ---Este comando nos sirva para adicionar archivos o carpetas nuevas al slage o a la versión local de mi equipo.
- * 2. git commint -m "comentario del commit" --- Este comando nos sirve para adicionar los cambios 
+ * 2. git commint -m "comentario del commit" --- Este comando nos sirve para adicionar los cambios..
  */
