3eb08fdc4bdbddef1495ce1ff37aee7c1e9f0ec2
MelanieB <melanieuwu28@gmail.com>
2020-08-25 14:00:14 -0500

    index configuración

---
+++ MABL/index.js
@@ -0,0 +1,5 @@
+/**
+* 1.git add, --> Este comando nos sirve para adicionar archivos o carpetas nuevas a la version local de mi equipo
+* 2.git commit -m nos sirve para adicionar cambios nuevos a un nuevo commit que adiccionatemos.
+* 3.push envia cambios al servidor
+ */
