2ce278b3b753e770a79c75d8f7f3397fb4dbf1b1
YulianaVP <yulianavargasp22@gmail.com>
2020-09-01 16:01:56 -0500

    archivo misfunciones y ejercicio

--- YVP/Funciones/misfunciones.js
+++ YVP/Funciones/misfunciones.js
@@ -1,11 +1,11 @@
 export const PI = 3.1416;
 
-function imprimir(numero1, numero2, suma) {
+export function imprimir(numero1, numero2, suma) {
     console.log("La suma de " + numero1 + " y " + numero2 + " es " + suma)
     console.log(`La suma de ${numero1} y ${numero2} es ${suma}`);
 }
 
-function esPar(suma) {
+export function esPar(suma) {
     if (suma % 2 == 0) {
         console.log(`${suma} es un numero par`)
     } else {
@@ -13,17 +13,30 @@ function esPar(suma) {
     }
 }
 
-function imprimirImpares1al100() {
-    for (contador = 1; contador <= 100; contador += 2) {
+export function imprimirImpares1al100() {
+    for (let contador = 1; contador <= 100; contador += 2) {
         console.log(contador);
     }
 }
 
-function imprimirNumeros1al100() {
+export function imprimirNumeros1al100() {
     let contador = 1;
     while (contador <= 100) {
         console.log(contador)
         //contador = contador + 1;
         contador++;
+    }
+}
+
+//Recibir 2 números, preguntar si son pares retornar la suma, si son impares retornar o imprimir la resta, sino retornar la multiplicación.
+
+export function DosNumeros() {
+    let num1 = 3, num2 = 7;
+    if (num1% 2 == 0 & num2 % 2 == 0){
+        console.log(`Suma: ${num1+num2}`)
+    }else if (num1% 2 == 1 & num2 % 2 == 1){
+        console.log(`Resta: ${num1-num2}`)
+    }else{
+        console.log(`Multiplicación: ${num1*num2}`)
     }
 }
--- YVP/index.js
+++ YVP/index.js
@@ -1,4 +1,4 @@
-import {PI} from './Funciones/misfunciones.js'
+import {PI, DosNumeros} from './Funciones/misfunciones.js'
 var numero1;
 let numero2;
 var suma;
@@ -10,7 +10,7 @@ numero2 = 300
 suma = numero1 + numero2;
 console.log("PI vale " + PI);
 
-// DosNumeros();
+DosNumeros();
 //esPar(4);
 //esPar(8);
 //esPar(20);
@@ -20,20 +20,3 @@ console.log("PI vale " + PI);
 
 // imprimir(3, 4, 56);
 // imprimir(numero1, numero2, suma);
-
-
-
-
-
-//Recibir 2 números, preguntar si son pares retornar la suma, si son impares retornar o imprimir la resta, sino retornar la multiplicación.
-
-// function DosNumeros() {
-//     let num1 = 3, num2 = 7;
-//     if (num1% 2 == 0 & num2 % 2 == 0){
-//         console.log(`Suma: ${num1+num2}`)
-//     }else if (num1% 2 == 1 & num2 % 2 == 1){
-//         console.log(`Resta: ${num1-num2}`)
-//     }else{
-//         console.log(`Multiplicación: ${num1*num2}`)
-//     }
-// }
