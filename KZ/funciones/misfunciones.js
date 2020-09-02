a3cac5ce9ae0c6b9c879f3d4811dc0fd9312e3ee
Katherine-Zapata <katherinezapataloaiza3@gmail.com>
2020-09-01 19:34:15 -0500

    Clases

--- KZ/funciones/misfunciones.js
+++ KZ/funciones/misfunciones.js
@@ -1,12 +1,13 @@
 
 export const PI= 3.1416;
 
-function imprimir(numero1, numero2, suma) {
+
+export function imprimir(numero1, numero2, suma) {
     console.log("la suma de " + numero1 + " y " + numero2 + " es " + suma);
     console.log(`la suma de ${numero1} y ${numero2}  es ${suma}`);
 }
 
-function esPar(suma) {
+export function esPar(suma) {
     if (suma % 2 == 0) {
         console.log(`${suma} es un numero par`)
     } else {
@@ -14,14 +15,14 @@ function esPar(suma) {
     }
 }
 
-function imprimirImpares1al100(){
+export function imprimirImpares1al100(){
     for(let contador = 1; contador <= 100; contador= contador + 2){
         console.log(contador);
     }
 }
 
 
-function ImprimirNumeros1al100(){
+export function ImprimirNumeros1al100(){
     let contador=1;
     while(contador <= 100){
 
@@ -30,9 +31,13 @@ function ImprimirNumeros1al100(){
     }
 }
 
-//N1yN2(3,5);
 
-function N1yN2(N1, N2){
+/**Crea una fucnión que reciba dos números un N1 o N2 ó A y B. Preguntar si los dos número son pares,
+ * se reotna la suma. Si los dos número son impares retorna la resta y si
+ * no se cumple esas dos regles la mulplicación de los dos*/
+
+//N1yN2(3,5);
+export function N1yN2(N1, N2){
    // var N1 = 1;
    // var N2 = 6;
 
--- KZ/index.js
+++ KZ/index.js
@@ -1,45 +1,45 @@
-/**
- * 1. git add . --- Este comando nos sirve para adicionar archivos o carpetas nuevas al stage o a la version local de mi equipo.
- * 2. git commit -m ""Comenario de commit--- Este comando nos sirve para adicionar los cambios nuevos o nuevo commit que enviaremos al respositorio.
- * */
-
-/*VARIABLES*/
-
-import {PI} from "./funciones/misfunciones.js"
-console.log("El valor de PI es " +PI)
-
-var numero1;
-let numero2;
-let suma;
+// 1. git add . --- Este comando nos sirve para adicionar archivos o carpetas nuevas al stage o a la version local de mi equipo.
+//2. git commit -m ""Comenario de commit--- Este comando nos sirve para adicionar los cambios nuevos o nuevo commit que enviaremos al respositorio.
 
 //con esots comandos se declara una variable (var, let, const)//
 //Para poner a correr las varibales, utilizamos "node .\index.js" //
 
-numero1 = 100;
+import {PI, N1yN2} from './funciones/misfunciones.js'
+import {PI,imprimiNumeros1al100, imprimirImpares1al100, N1yN2} from './funciones/misfunciones.js'
+import {Moto} from './clases/Moto.js'
+var numero1;
+let numero2, suma;
+
+numero1 = 33;
 numero2 = 300;
 
-
 suma = numero1 + numero2;
-console.log("PI vale: " + PI)
-
-
+console.log("PI vale " + PI);
+//imprimiNumeros1al100();
 //imprimirImpares1al100();
+//N1yN2();
+let akt = new Moto(4, 6);
+let yamaha = new Moto(5, 4);
+yamaha.rodar()
+console.log(akt)
+console.log(yamaha)
 
-//esPar(5);
-//esPar(4);
-//esPar(47);
-//esPar(400);
+//console.log("PI vale " + PI);
+// imprimirImpares1al100();
+// console.log("la suma de 3 y 5 es "  + sumar(3, 5));
+/*esPar(4);
+esPar(40);
+esPar(8);
+esPar(41);
+esPar(5);
+esPar(1);
+esPar(0);
+esPar(434534234);
+esPar(437723472734);
+imprimir(3, 4, 56);
+imprimir(numero1, numero2, suma); */
 
-//imprimir (3,4, 56);
-//imprimir (numero1,  numero2, suma); 
 
-//imprimir los numeros del 1 al 100//
-
-
-
-/**Crea una fucnión que reciba dos números un N1 o N2 ó A y B. Preguntar si los dos número son pares,
- * se reotna la suma. Si los dos número son impares retorna la resta y si
- * no se cumple esas dos regles la mulplicación de los dos*/
 
 
 
