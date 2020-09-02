b4f4172e6f57c78114887f270279e58cea68c830
Katherine-Zapata <katherinezapataloaiza3@gmail.com>
2020-09-01 20:51:35 -0500

    Nueva modificación

--- KZ/index.js
+++ KZ/index.js
@@ -4,21 +4,17 @@
 //con esots comandos se declara una variable (var, let, const)//
 //Para poner a correr las varibales, utilizamos "node .\index.js" //
 
-import {PI} from './funciones/misfunciones.js'
-import { PI, imprimiNumeros1al100, imprimirImpares1al100, N1yN2 } from './funciones/misfunciones.js'
+import { PI, imprimirImpares1al100, imprimirNumeros1al100, N1yN2 } from './funciones/misfunciones.js'
 import { Moto } from './clases/Moto.js'
-var numero1;
-let numero2, suma;
 
-numero1 = 33;
+var numero1;
+let numero2;
+let suma;
+
+numero1 = 203;
 numero2 = 300;
 
 suma = numero1 + numero2;
-//console.log("PI es " + PI);
-
-//imprimiNumeros1al100();
-//imprimirImpares1al100();
-//N1yN2();
 
 let akt = new Moto(4, 6);
 let yamaha = new Moto(5, 4);
@@ -27,20 +23,19 @@ console.log(akt)
 console.log(yamaha)
 
 //console.log("PI vale " + PI);
-// imprimirImpares1al100();
-// console.log("la suma de 3 y 5 es "  + sumar(3, 5));
-/*esPar(4);
-esPar(40);
-esPar(8);
-esPar(41);
-esPar(5);
-esPar(1);
-esPar(0);
-esPar(434534234);
-esPar(437723472734);
+//imprimirImpares1al100();
+//imprimirNumeros1al100();
+//N1yN2();
 
-imprimir(3, 4, 56);
-imprimir(numero1, numero2, suma); */
+// esPar(5);
+// esPar(4);
+// esPar(47);
+// esPar(400);
+
+// imprimir(3, 4, 56);
+// imprimir(numero1, numero2, suma);
+
+//imprimir los números del 1 al 100
 
 
 
