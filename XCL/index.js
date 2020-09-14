570ac5ffedc3ab1bda8b578e52a741ce53ef04ad
ximelopez06 <ximel862@gmail.com>
2020-09-14 18:49:01 -0500

    Modificaciones y intento de tarea

--- XCL/index.js
+++ XCL/index.js
@@ -1,41 +1,54 @@
-import {PI} from "./funciones/misfunciones.js"
-import {Moto} from './clases/Moto.js'
-var numero1;
-let numero2, suma;
+ const express = require (`express`) 
+ const app = express()
 
-numero1 = 33;
-numero2 = 300;
+ app.get(`/`, function(req,res){
+    res.send("Hola estudiantes!!!!")
+})
 
-suma = numero1 + numero2;
-let akt = new Moto(4, 6);
-let yamaha = new Moto(5, 4);
-yamaha.rodar()
-console.log(akt)
-console.log(yamaha)
-// console.log("PI vale:" + PI)
+app.get(`/estudiante/:documento`, function(req,res){
+    console.log("El documento es", req.params.documento)
+    res.send("Servicio para mostar un solo estudiante con documento #" +req.params.documento)
+})
 
-//DosNumeros();
-//esPar(4);
-//esPar(8);
-//esPar(20);
-//esPar(5);
-//esPar(9);
-//esPar(45);
+ app.get(`/estudiantes`, function(req,res){
+    // res.send("<h1>Servicio para la lista de estudiantes</h1>")
+    res.json ({nombre:"ximena", edad:"16"})
+ })
 
-// imprimir(3, 4, 56);
-// imprimir(numero1, numero2, suma);
+ app.listen(8080,function(){
+     console.log("El servidor escuchando en puerto 8080")
+ })
 
+ //Tarea desarrollar un midelware individualmente, con la siguiente ruta /numeros primos/ : limite hasta donde quiero imprimir los numeros
+// el midelware debe imprimir en el navegador (puede ser json o string) con los numero primos
+// hasta el limite donde se le indico el parametro (:limite)
+//subir todos los cambios en el github para su revision y anexar la url de la carpeta del github en el clasrrom de la actividad propesta
 
-//Recibir 2 números, preguntar si son pares retornar la suma, si son impares retornar o imprimir la resta, sino retornar la multiplicación.
+app.get(`/numeros`, function(req,res){
+    var c = 100;
+var j = 2;
+var numerosPrimos = [];
 
-// //Ejercico correcion
-//N1yN2(1, 6);
-//function N1yN2(N1, N2) {
-    // if (N1 % 2 == 0 && N2 % 2 == 0) {
-    //     console.log(`suma: ${N1 + N2}`)
-    // } else if (N1 % 2 != 0 && N2 % 2 != 0)
-    //     console.log(`resta: ${N1 - N2}`)
-    //     } else {
-    //     console.log(`mueltiplicacion: ${N1 * N2}`)
-    //     }
+for (; j < c; j++) {
 
+  if (primo(j)) {
+    numerosPrimos.push(j);
+  }
+  
+}
+
+console.log(numerosPrimos);
+
+function primo(numero) {
+
+  for (var i = 2; i < numero; i++) {
+
+    if (numero % i === 0) {
+      return false;
+    }
+
+  }
+
+  return numero !== 1;
+}
+})
