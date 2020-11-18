eccc1449c8867bf02674e4f728ab7471f3461af3
ximelopez06 <ximel862@gmail.com>
2020-11-18 17:04:36 -0500

    Midelware

---
+++ MME/Funciones/misfunciones.js
@@ -0,0 +1,44 @@
+export const PI = 3.1419383;
+
+export function imprimir(numero1, numero2, suma) {
+    console.log("La suma de " + numero1 + ' y ' + numero2 + " es " + suma);
+    console.log(`La suma de ${numero1} y ${numero2} es ${suma}`);
+}
+
+export function esPar(suma) {
+    if (suma % 2 == 0) {
+        console.log(`${suma} es un numero par`)
+    } else {
+        console.log(`${suma} es un numero impar`)
+    }
+}
+
+
+export function imprimirImpares1al100(){
+    for(let contador = 1; contador <= 100; contador += 2){
+        console.log(contador);
+    }
+}
+
+export function imprimiNumeros1al100(){
+    let contador = 1;
+    while(contador <= 100){
+        console.log(contador);
+        //contador = contador + 1;
+        contador++;
+    }
+}
+
+export function sumar(a, b){
+    return a + b
+} 
+ 
+function DosN(N1, N2) {
+    if (N1% 2 == 0 && N2% 2 == 0){
+        console.log(`la Suma es: ${N1+N2}`)
+    }else if (N1% 2 != 0 && N2 % 2 != 0){
+        console.log(`la Resta es: ${N1-N2}`)
+    }else{
+        console.log(`la Multiplicación es: ${N1*N2}`)
+    }
+}
